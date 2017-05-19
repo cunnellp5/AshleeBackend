51dda69039b009cec8e8eb73830b38d69670263d
cunnellp5 <philip.cunnell@colorado.edu>
2017-05-19 16:48:41 -0600

    slash

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors({
-  origin: 'https://ashleesofia-cb29b.firebaseapp.com/',
+  origin: 'https://ashleesofia-cb29b.firebaseapp.com',
   credentials: true
 }));
 
