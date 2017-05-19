03ababddea2785ebfc9ac465175f7611780df1a8
cunnellp5 <philip.cunnell@colorado.edu>
2017-05-19 12:03:26 -0600

    added headers

--- routes/user.js
+++ routes/user.js
@@ -9,6 +9,11 @@ const knex = require('../db/knex');
 //     res.json(data)
 //   });
 // });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 router.get('/:id', (req, res) => {
   if (!isNaN(req.params.id) && req.user.id == req.params.id) {
