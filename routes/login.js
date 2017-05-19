45118cb4cf3f1bbe0f5595cfa3aaa9e9ce3f7f6d
cunnellp5 <philip.cunnell@colorado.edu>
2017-05-19 12:03:26 -0600

    added headers

--- routes/login.js
+++ routes/login.js
@@ -6,6 +6,11 @@ var jwt = require('jsonwebtoken');
 
 require('dotenv').config();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 router.post('/', function(req, res, next) {
   if (!req.body.password || !req.body.email) {
